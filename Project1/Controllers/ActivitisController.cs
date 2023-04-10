850aa5d7be91589ae2c7bfb4631464ff4cfb410e
amir momenzadeh <amirmomenzade@outlook.com>
2023-04-10 22:21:15 +0330

    baseProject

---
+++ Project1/Controllers/ActivitisController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Project1.Controllers
+{
+    public class ActivitisController : BaseApiController
+    {
+
+        private readonly DataContext _db;
+        public ActivitisController(DataContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetAllActivity()
+        {
+            return await _db.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            return await _db.Activities.FindAsync(id);
+        }
+
+    }
+}
